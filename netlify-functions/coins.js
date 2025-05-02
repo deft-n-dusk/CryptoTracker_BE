eb64e7c5c8cf44a47863872d0f4c9b930e67d189
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-05-03 00:29:35 +0530

    Added netlify-functions

---
+++ netlify-functions/coins.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  try {
+    const response = await axios.get(
+      'https://api.coingecko.com/api/v3/coins/markets',
+      {
+        params: {
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 1,
+          sparkline: false
+        }
+      }
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch data from CoinGecko' })
+    };
+  }
+};
